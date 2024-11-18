ef9a90cc92545b87d82b0bc53627ebafae0bf0f7
Daniel Loughmiller <41339732+danloughmiller@users.noreply.github.com>
2024-11-17 22:01:49 -0500

    Added etension points to input service

--- Runtime/PlayerInputService.cs
+++ Runtime/PlayerInputService.cs
@@ -1,42 +1,128 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
 
 namespace DGP.StackableInput
 {
-    public abstract class PlayerInputService<TInputActionAsset, TGameInputMode> where TInputActionAsset : IInputActionCollection where TGameInputMode : class, IGameInputMode<TInputActionAsset>
+    public abstract class PlayerInputService<TInputActionAsset, TGameInputMode> : IDisposable where TInputActionAsset : IInputActionCollection where TGameInputMode : class, IGameInputMode<TInputActionAsset>
     {
-        private readonly TInputActionAsset gameInput;
-        private readonly Stack<TGameInputMode> inputModeStack = new();
+        private bool _disposed;
+        
+        protected readonly TInputActionAsset GameInput;
+        private readonly Stack<TGameInputMode> _inputModeStack = new();
 
         protected PlayerInputService(TInputActionAsset gameInput) {
-            this.gameInput = gameInput;
+            this.GameInput = gameInput;
+        }
+        
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
 
-        ~PlayerInputService() {
-            gameInput.Disable();
-            
-            while (inputModeStack.Count > 0)
-                PopInputMode();
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+        
+            if (disposing)
+            {
+                GameInput.Disable();
+                while (_inputModeStack.Count > 0)
+                    PopInputMode();
+            }
+        
+            _disposed = true;
         }
         
-        public TGameInputMode CurrentInputMode => inputModeStack.Count > 0 ? inputModeStack.Peek() : null;
+        public TGameInputMode CurrentInputMode => _inputModeStack.Count > 0 ? _inputModeStack.Peek() : null;
         
         public void PushInputMode(TGameInputMode nextMode) {
-            if (inputModeStack.TryPeek(out TGameInputMode currentMode))
-                currentMode.ExitGameInputMode(gameInput);
+            if (nextMode == null)
+                throw new ArgumentNullException(nameof(nextMode));
             
-            inputModeStack.Push(nextMode);
-            nextMode.EnterGameInputMode(gameInput);
+            if (!ValidateInputModeChange(nextMode))
+                return;
+            
+            OnBeforePushInputMode(nextMode);
+            
+            if (_inputModeStack.TryPeek(out TGameInputMode currentMode)) {
+                currentMode.ExitGameInputMode(GameInput);
+                OnInputModeExited(currentMode);
+            }
+            
+            _inputModeStack.Push(nextMode);
+            nextMode.EnterGameInputMode(GameInput);
+            OnInputModeEntered(nextMode);
+            
+            OnAfterPushInputMode(nextMode);
         }
         
         public void PopInputMode() {
-            if (inputModeStack.TryPeek(out TGameInputMode currentMode)) {
-                currentMode.ExitGameInputMode(gameInput);
-                inputModeStack.Pop();
+            if (!_inputModeStack.TryPeek(out TGameInputMode currentMode))
+                return;
+            
+            if (!ValidateInputModePop(currentMode))
+                return;
+            
+            OnBeforePopInputMode(currentMode);
+            
+            currentMode.ExitGameInputMode(GameInput);
+            OnInputModeExited(currentMode);
+            _inputModeStack.Pop();
+            
+            if (_inputModeStack.TryPeek(out TGameInputMode nextMode)) {
+                nextMode.EnterGameInputMode(GameInput);
+                OnInputModeEntered(nextMode);
             }
             
-            if (inputModeStack.TryPeek(out TGameInputMode nextMode))
-                nextMode.EnterGameInputMode(gameInput);
+            OnAfterPopInputMode(currentMode);
         }
+        
+        #region Extension Points
+        /// <summary>
+        /// Validates whether the specified input mode can be pushed onto the stack.
+        /// </summary>
+        /// <param name="nextMode">The mode being pushed</param>
+        /// <returns>True if the mode can be pushed, false otherwise</returns>
+        protected virtual bool ValidateInputModeChange(TGameInputMode nextMode) => true;
+
+        /// <summary>
+        /// Validates whether the current input mode can be popped from the stack.
+        /// </summary>
+        /// <param name="currentMode">The mode being popped</param>
+        /// <returns>True if the mode can be popped, false otherwise</returns>
+        protected virtual bool ValidateInputModePop(TGameInputMode currentMode) => true;
+
+        /// <summary>
+        /// Called before a new input mode is pushed onto the stack.
+        /// </summary>
+        protected virtual void OnBeforePushInputMode(TGameInputMode nextMode) { }
+
+        /// <summary>
+        /// Called after a new input mode has been pushed onto the stack.
+        /// </summary>
+        protected virtual void OnAfterPushInputMode(TGameInputMode nextMode) { }
+
+        /// <summary>
+        /// Called before the current input mode is popped from the stack.
+        /// </summary>
+        protected virtual void OnBeforePopInputMode(TGameInputMode currentMode) { }
+
+        /// <summary>
+        /// Called after an input mode has been popped from the stack.
+        /// </summary>
+        protected virtual void OnAfterPopInputMode(TGameInputMode previousMode) { }
+
+        /// <summary>
+        /// Called when an input mode becomes active.
+        /// </summary>
+        protected virtual void OnInputModeEntered(TGameInputMode mode) { }
+
+        /// <summary>
+        /// Called when an input mode becomes inactive.
+        /// </summary>
+        protected virtual void OnInputModeExited(TGameInputMode mode) { }
+        #endregion
     }
 }
